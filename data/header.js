304d8d88b440b7beb42f6fbd40fcad485a147dab
Modjtaba Rouhani <47209178+modrouhani@users.noreply.github.com>
2025-03-08 13:33:36 +0330

    Update header.js

--- data/header.js
+++ data/header.js
@@ -3,7 +3,7 @@ export const header = {
   website: "Deep Learning and Neural Networks",
   institute:
     '<span class="text-red-light">F</span>erdowsi <span class="text-red-light">U</span>niversity Of <span class="text-red-light">M</span>ashhad',
-  course: '<span class="text-red-light">Course</span> Name',
+  course: '<span class="text-red-light">Deep Learning</span>',
   background:
     "https://s3.amazonaws.com/hoth.bizango/images/797280/cloud-computing-msc_feature.jpg",
 };
